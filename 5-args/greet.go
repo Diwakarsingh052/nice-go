dce6c35e068d257157c9c39d66b7f1278f4fbab8
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2023-09-04 14:03:48 +0530

    .

--- 5-args/greet.go
+++ 5-args/greet.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
 func main() {
+	greet()
+	fmt.Println("end of the main")
+}
+
+func greet() {
+	//2nd
+	data := os.Args[1:]
+	if len(data) != 3 {
+		log.Println("please provide name , age , marks")
+		//os.Exit(1)
+		return // stops the exec of the current func
+	}
+
+	name := data[0]
+	ageString := data[1]
+	//var err error // default value is nil // indicates no error
+	//fmt.Println(err)
+	age, err := strconv.Atoi(ageString)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Println(name, age)
 
 }
