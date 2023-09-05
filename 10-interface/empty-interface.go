9f977ebf0b6f3a32a8b68f2ec8005623c72466d0
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2023-09-05 16:58:54 +0530

    .

--- 10-interface/empty-interface.go
+++ 10-interface/empty-interface.go
@@ -1,9 +1,28 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+
 	var i any = 10
+
 	i = "hello"
 
-	var s string = i // use type assertion and make this line work
+	//i = 1000
 
+	var s string
+
+	s, ok := i.(string) // use type assertion and make this line work
+
+	if !ok {
+
+		fmt.Println("i Is not a string")
+
+		return
+
+	}
+
+	fmt.Println("s has value : ", s)
 }
