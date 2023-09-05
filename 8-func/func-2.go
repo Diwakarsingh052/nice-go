82b83acc83856e299c17881991f8191e735ce2f2
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2023-09-05 09:32:21 +0530

    .

---
+++ 8-func/func-2.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
+
+func SumString(s, x string) (int, error) { // err must be the last value to be returned
+
+	a, err := strconv.Atoi(s)
+	if err != nil {
+		return 0, err
+	}
+
+	b, err := strconv.Atoi(x)
+	if err != nil {
+		return 0, err
+	}
+
+	return a + b, nil //success
+
+}
+
+func main() {
+	sum, err := SumString("a", "20")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Println(sum)
+}
